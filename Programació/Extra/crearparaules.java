9b8844cc371253de0c8dc0eb128c9755dd83bace
TheBlackCat25 <claudiodragomir25@gmail.com>
2019-11-14 17:20:01 +0100

    Crear Paraules Java

--- Programació/Extra/crearparaules.java
+++ Programació/Extra/crearparaules.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 /**
  * crearparaules
  */
 public class crearparaules {
 
-    static String[] jugadors;
-
     public static void main(String[] args) {
-        
+        ArrayList <String> al = new ArrayList<String>();
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0; i<2;i++){
+            System.out.println("Introdueix el nom del jugador "+(i+1));
+            al.add(sc.next());
+        }
     }
 }
